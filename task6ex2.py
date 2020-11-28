1677df8d184b09a8f18c429093ca55faa5a01486
Nanditha. U <74827999+Nanditha163@users.noreply.github.com>
2020-11-28 14:19:58 +0530

    Update task6ex2.py

--- task6ex2.py
+++ task6ex2.py
@@ -3,3 +3,10 @@ for i in range(0,row+1):
     for j in range(row-i,0,-1):
         print(j,end=" ")
     print()
+    
+    OUTPUT:
+        5 4 3 2 1 
+        4 3 2 1 
+        3 2 1 
+        2 1 
+        1 
