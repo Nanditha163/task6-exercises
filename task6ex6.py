fa84743e1cb54a442de0bbba34ea2d107f2e951c
Nanditha. U <74827999+Nanditha163@users.noreply.github.com>
2020-11-28 14:26:37 +0530

    Update task6ex6.py

--- task6ex6.py
+++ task6ex6.py
@@ -3,3 +3,13 @@ if num>0:
     print(num,"It is positive!")
 else:
     print(num,"It is negative")
+    
+    
+    OUTPUT:
+        
+        1.    Enter a number:16
+              16 It is positive!
+                
+                
+        2.    Enter a number:-21
+              -21 It is negative        
