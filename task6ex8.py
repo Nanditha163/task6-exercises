17e7b40c1c2d02cb39478f2e62fc357c6f007827
Nanditha. U <74827999+Nanditha163@users.noreply.github.com>
2020-11-28 14:29:23 +0530

    Update task6ex8.py

--- task6ex8.py
+++ task6ex8.py
@@ -3,3 +3,9 @@ x=0.90
 print("math.cos(",x,"):",math.cos(x));
 print("math.sin(",x,"):",math.sin(x));
 print("math.tan(",x,"):",math.tan(x));
+
+
+OUTPUT:
+math.cos( 0.9 ): 0.6216099682706644
+math.sin( 0.9 ): 0.7833269096274834
+math.tan( 0.9 ): 1.2601582175503392
